7c179c4a1cde072bd9895e678cf25682512cbcbb
Mohit Singh <mohit.singh@weboniselab.com>
2013-07-08 14:20:59 +0530

    latest version of shop inventry assignment

--- shop_inventry1.2/product.rb
+++ shop_inventry1.2/product.rb
@@ -6,11 +6,11 @@ module Bridge
       @stock_item
  			@comp_name
 
-   	attr_accessor:id
-    attr_accessor:pro_name
-    attr_accessor:price
-    attr_accessor:stock_item
-    attr_accessor:comp_name
+   		attr_accessor:id
+    	attr_accessor:pro_name
+    	attr_accessor:price
+    	attr_accessor:stock_item
+    	attr_accessor:comp_name
 
     def add_product?(a)
 	 		d="  |  "
@@ -19,9 +19,7 @@ module Bridge
       file.write("\n")
  			file.close
 			return true
-
     end
-
 
     def remove_product?(a)
 			str=""
@@ -41,8 +39,6 @@ module Bridge
 				File.rename("abc.txt","product_record.txt")
     end
 
-
-
     def list_product()
       puts "ID | Name | Price | Stock item | Compnay"
 			File.open("product_record.txt", "r").each_line do |line|
@@ -52,21 +48,16 @@ module Bridge
 			 puts "\n\n"
     end
 
-
-
     def search_product(b)
 			val=b.pro_name
     	File.open("product_record.txt") do |f|
   		f.each_line do |line|
    		if line.include? "#{val}" 
       	puts "#{line}"
-   	 end
- 			 end
+   				end
+ 			 	end
 			end
     end
-
-
-
 
     def update_product(a)
 			str=""
@@ -79,17 +70,14 @@ module Bridge
 			else
         str << line	
 				str << "\n"				
+      		end
+      	end
       end
-       end
-        end
-					#str.gsub /\n+/, ''
 					puts "#{str}"
 					File.open("abc.txt", 'w+') {|f| f.write(str) }
 					File.delete("product_record.txt")
 					File.rename("abc.txt","product_record.txt")
 					return true
     end
-
   end
-
 end
